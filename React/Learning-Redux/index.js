45f6d5fd71e2db563166536d05592e91b7e7e9da
syedmasum <syed.develops@gmail.com>
2023-07-06 13:36:52 +0530

    a commit before changine initFuc to pending, fulfilled and rejected

--- React/Learning-Redux/index.js
+++ React/Learning-Redux/index.js
@@ -1,32 +1,89 @@
 //imports:
-import { createStore } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import axios from "axios";
+
 //actions and payloads:
-const incByAmt = "incByAmt";
-const inc = "inc";
-const dec = "dec";
+//account actions
+const accInit = "account/init";
+const incByAmt = "account/incrementByAmount";
+const inc = "account/increment";
+const dec = "account/decrement";
+const getUserPending = "account/getUser/pending";
+const getAccUserFulfilled = "account/getUser/fulfilled";
+const getAccUserRejected = "account/getUser/rejected";
+
+//bouns actions
+const incBonus = "bonus/increment";
 
 // creating the store
-const store = createStore(reducer, thunk.default);
+const store = createStore(
+  combineReducers({ account: accountReducer, bonus: bonusReducer }),
+  applyMiddleware(thunk.default)
+);
 
-function reducer(state = { amount: 0 }, action) {
+// Action Creators
+function initFunc() {
+  return { type: accInit };
+}
+function incFunc() {
+  return { type: inc };
+}
+function decFunc() {
+  return { type: dec };
+}
+function incByAmtfunc(amt) {
+  return { type: incByAmt, payload: amt };
+}
+function getUser(id) {
+  return async (dispatch, state) => {
+    try {
+      const { data } = await axios.get(`http://localhost:3000/users/${id}`);
+      // dispatch(incByAmtfunc(data.amount));
+      dispatch(incFunc());
+    } catch (err) {
+      console.log(err);
+    }
+  };
+}
+//bonus actions
+function incBonusFunc() {
+  return { type: incBonus };
+}
+
+// Reducers
+function accountReducer(state = { amount: 0 }, action) {
   switch (action.type) {
     case inc: {
       return { amount: state.amount + 1 };
     }
     case dec: {
-      return { amount: state.amount + 1 };
+      return { amount: state.amount - 1 };
     }
     case incByAmt: {
       return { amount: state.amount + action.payload };
+    }
+    case accInit: {
+      return { amount: state.amount + 1 };
+    }
+    default:
+      return state;
+  }
+}
+function bonusReducer(state = { points: 0 }, action) {
+  switch (action.type) {
+    case incBonus: {
+      return { points: state.points + 1 };
     }
     default:
       return state;
   }
 }
 
+// loging the state
 store.subscribe(() => console.log(store.getState()));
 
+// dispatching action
 setInterval(() => {
-  store.dispatch({ type: inc });
+  store.dispatch(getUser(1));
 }, 2000);
